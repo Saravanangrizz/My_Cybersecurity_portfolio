03e5ef9c7040c963e7c38bf348f716ac0382d487
Saro <112953065+Saravanangrizz@users.noreply.github.com>
2025-05-03 23:00:10 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,1 +1,26 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import { motion } from "framer-motion";
+
+const About = () => (
+  <div className="page">
+    <Navbar />
+    <motion.div
+      className="content"
+      initial={{ y: 100, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.8 }}
+    >
+      <h2>About Me</h2>
+      <p>
+        I'm C. Saravanan, a cybersecurity enthusiast driven by curiosity to
+        understand how systems can be broken — and how to secure them. With a
+        background in ethical hacking and log analysis, I thrive on solving real
+        cyber problems and preventing digital threats before they strike.
+      </p>
+    </motion.div>
+  </div>
+);
+
+export default About;
 
