41bc06e6a2d64409f9f4d12e39c8139ec6fb678f
Saro <112953065+Saravanangrizz@users.noreply.github.com>
2025-05-03 22:59:30 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,1 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h2 className="logo">C.Saravanan</h2>
+    <div className="links">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/projects">Projects</Link>
+      <Link to="/skills">Skills</Link>
+      <Link to="/contact">Contact</Link>
+    </div>
+  </nav>
+);
+
+export default Navbar;
 
